b6440b29de15476aa3cb54e8c8beee35ae1d29a1
suhomin <msuho16@gmail.com>
2021-07-29 15:14:33 +0900

    코로나 정보 크롤링

--- caapi.py
+++ caapi.py
@@ -4,104 +4,38 @@ import requests
 import xmltodict
 import json
 import time
+import lxml
+from bs4 import BeautifulSoup
+
 
 
 def api1():
-    # 오늘 코로나 데이터 가져오기
-    now = date.today()
-    now_str = now.strftime("%Y%m%d")
+    # 코로나 데이터 가져오기
 
-    url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
+    url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
 
-    params = {
-        "serviceKey" : "zD0l/q+zOqlqv7br6BYklo1jxWZqyzg3BO1lS/p7loirhUlVFFGXt299tnoRFi78ThzPQP4qTZOKPMTmQwst3A==",
-        "pageNo" : "1",
-        "numOfRows" : 10,
-        "startCreateDt" : now_str,
-        "endCreateDt" : now_str
-    }
+    html = requests.get(url).text
 
-    res = requests.get(url, params=params)
+    bs = BeautifulSoup(html, 'lxml')
 
-    dt = xmltodict.parse(res.text)
-    jo = json.dumps(dt)
-    data1 = json.loads(jo)
-    if data1['response']['body']['totalCount'] == "0":
-        data = False
-    else:
-        data = data1['response']['body']['items']['item']
-
-    if not data:
-        yday = now - timedelta(days=1)
-        yday_str = yday.strftime("%Y%m%d")
-
-        url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
-
-        params = {
-            "serviceKey" : "zD0l/q+zOqlqv7br6BYklo1jxWZqyzg3BO1lS/p7loirhUlVFFGXt299tnoRFi78ThzPQP4qTZOKPMTmQwst3A==",
-            "pageNo" : "1",
-            "numOfRows" : 10,
-            "startCreateDt" : yday_str,
-            "endCreateDt" : yday_str
-        }
-
-        res = requests.get(url, params=params)
-
-        dt = xmltodict.parse(res.text)
-        jo = json.dumps(dt)
-        data1 = json.loads(jo)
-        if data1['response']['body']['totalCount'] == "0":
-            data = False
-        else:
-            data = data1['response']['body']['items']['item']
-
-    # 어제 코로나 데이터 가져오기
-    yday = now - timedelta(days=1)
-    yday_str = yday.strftime("%Y%m%d")
-
-    url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
-
-    params = {
-        "serviceKey" : "zD0l/q+zOqlqv7br6BYklo1jxWZqyzg3BO1lS/p7loirhUlVFFGXt299tnoRFi78ThzPQP4qTZOKPMTmQwst3A==",
-        "pageNo" : "1",
-        "numOfRows" : 10,
-        "startCreateDt" : yday_str,
-        "endCreateDt" : yday_str
-    }
-
-    res = requests.get(url, params=params)
-
-    dt = xmltodict.parse(res.text)
-    jo = json.dumps(dt)
-    data1 = json.loads(jo)
-    if data1['response']['body']['totalCount'] == "0":
-        data2 = False
-    else:
-        data2 = data1['response']['body']['items']['item']
-
-    #어제,오늘 데이터 가공
-    ndayca1 = {
-        '확진자수':data['decideCnt'],
-        '격리해제수':data['clearCnt'],
-        '검사진행수':data['examCnt'],
-        '사망자수':data['deathCnt'],
-    }
-    ydayca = {
-        '확진자수':data2['decideCnt'],
-        '격리해제수':data2['clearCnt'],
-        '검사진행수':data2['examCnt'],
-        '사망자수':data2['deathCnt'],
-    }
+    총확진자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(1) > ul > li:nth-child(1) > dl > dd').text.replace(',','')))
+    확진자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(1) > ul > li:nth-child(2) > dl > dd > ul > li:nth-child(1) > p').text.replace('+','').replace(' ','').replace(',','')))
+    총완치자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(2) > ul > li:nth-child(1) > dl > dd').text.replace(',','')))
+    완치자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(2) > ul > li:nth-child(2) > dl > dd > span').text.replace('+','').replace(' ','').replace(',','')))
+    총검사자 = int(float(bs.select_one('#content > div > div.data_table.mgt16.mini > table > tbody > tr > td:nth-child(8)').text.replace(',','')))
+    검사자 = int(float(bs.select_one('#content > div > div:nth-child(12) > table > tbody > tr > td:nth-child(1) > span').text.replace(',','')))
+    총사망자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(4) > ul > li:nth-child(1) > dl > dd').text.replace(',','')))
+    사망자 = int(float(bs.select_one('#content > div > div.caseTable > div:nth-child(3) > ul > li:nth-child(2) > dl > dd > span').text.replace('+','').replace(' ','').replace(',','')))
 
     ndayca = {
-        '확진자수':data['decideCnt'],
-        '신규확진자수':int(ndayca1['확진자수']) - int(ydayca['확진자수']),
-        '격리해제수':data['clearCnt'],
-        '신규격리해제수':int(ndayca1['격리해제수']) - int(ydayca['격리해제수']),
-        '검사진행수':data['examCnt'],
-        '신규검사진행수':int(ndayca1['검사진행수']) - int(ydayca['검사진행수']),
-        '사망자수':data['deathCnt'],
-        '신규사망자수':int(ndayca1['사망자수']) - int(ydayca['사망자수']),
+        '확진자수':총확진자,
+        '신규확진자수':확진자,
+        '격리해제수':총완치자,
+        '신규격리해제수':완치자,
+        '검사진행수':총검사자,
+        '신규검사진행수':검사자,
+        '사망자수':총사망자,
+        '신규사망자수':사망자,
     }
     
     file_path = 'cadata1.json'
@@ -216,10 +150,5 @@ def api2():
 
     
 if __name__ == '__main__':
-    while(1):
-        api2()
-        api1()
-        print(1)
-        time.sleep(1800)
-    # print(api1())
-    # print(api2())
+    print(api1())
+    print(api2())
